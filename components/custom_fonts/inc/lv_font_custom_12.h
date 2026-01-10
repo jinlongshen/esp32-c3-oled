6c7279ba7ec79fc804a983e1bf86839c4bb3449c
Jinlong Shen <shenjinlong@yahoo.com>
2026-01-10 03:11:24 +0100

    added lvgl custom character

---
+++ components/custom_fonts/inc/lv_font_custom_12.h
@@ -0,0 +1,17 @@
+#ifndef COMPONENTS_FONTS_INC_LV_FONT_CUSTOM_12_H
+#define COMPONENTS_FONTS_INC_LV_FONT_CUSTOM_12_H
+
+#include "lvgl.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+extern const lv_font_t lv_font_custom_12;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // COMPONENTS_FONTS_INC_LV_FONT_CUSTOM_12_H
